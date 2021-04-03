512e74eed841ab18bb1a6ee8a35b0614574378b9
Antonio García <agarcia@shsconsultores.es>
2021-04-04 00:44:03 +0200

    cambios iniciales y acces token

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SobreMiComponent } from './pages/sobre-mi/sobre-mi.component';
+import { TrabajosComponent } from './pages/trabajos/trabajos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sobre-mi', component: SobreMiComponent },
+  { path: 'trabajos', component: TrabajosComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
