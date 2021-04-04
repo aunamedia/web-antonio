36241d21e132fb1d7095e6295ec3d9a1aa149376
Antonio García <agarcia@shsconsultores.es>
2021-04-04 06:54:42 +0200

    home y fallo test provocado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'web-antonio';
+  title = 'web-antonio4';
   // n: Number = fn();
 }
 
