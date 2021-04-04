fa52a71957bf4cc1fecb42b5db868ac2192c961b
Antonio García <agarcia@shsconsultores.es>
2021-04-04 07:14:07 +0200

    sin fallo de test, para CI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'web-antonio4';
+  title = 'web-antonio';
   // n: Number = fn();
 }
 
